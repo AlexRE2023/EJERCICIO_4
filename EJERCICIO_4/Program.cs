1b35a07dcdb181dfda5bd273b620db83724ed3a2
alexi <alexi@ALEXANDER>
2023-08-27 14:48:04 -0500

    Agregar archivos de proyecto.

---
+++ EJERCICIO_4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Ingrese el número de alumnos de la sección A, B y C respectivamente");
+int A = int.Parse(Console.ReadLine());
+int B = int.Parse(Console.ReadLine());
+int C = int.Parse(Console.ReadLine());
+
+int horas = (A + B + C * 6) / 60;
+int minutos = A % 5 + B % 8 + C % 6;
+
+Console.WriteLine(horas + " horas y " + minutos + " minutos");
